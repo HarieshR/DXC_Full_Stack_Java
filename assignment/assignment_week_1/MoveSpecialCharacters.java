640aefe60f7113d830cd2b4ae67af65adca47e03
HarieshR <harieshr161@gmail.com>
2023-02-03 14:54:11 +0530

    week_1_String

---
+++ assignment/assignment_week_1/MoveSpecialCharacters.java
@@ -0,0 +1,34 @@
+package assignment_week_1;
+import java.util.*;
+
+class msc{
+	private String m;
+
+	public msc(String m) {
+		this.m = m;
+	}
+
+	public String getMsc() {
+		String sc = m.replaceAll("[A-Za-z0-9]","");
+		String a = m.replaceAll("[^A-Za-z0-9]","");
+		return a+sc;
+	}
+
+	public void setMsc(String m) {
+		this.m = m;
+	}	
+}
+
+public class MoveSpecialCharacters{
+	
+		public static void main(String[] args) {
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter a string: ");
+			String in = sc.nextLine();
+			msc op = new msc(in);
+			
+			System.out.println(op.getMsc());
+		}
+	}
+
