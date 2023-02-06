f03440237db2454d6175322a3095e40c45184005
Hariesh R <86884890+HarieshR@users.noreply.github.com>
2023-02-06 21:48:08 +0530

    Delete assessment/assessment01/assessment 01 directory

--- assessment/assessment01/assessment 01/DXCCricketTournamentTester.java
+++
@@ -1,8 +0,0 @@
-package assessment;
-public class DXCCricketTournamentTester {
-	public static void main(String[] args) {
-	    DXCCricketTournament in = new DXCCricketTournament();
-	    in.DXCCricketTournamentDetails();
-	    }
-	}
-
