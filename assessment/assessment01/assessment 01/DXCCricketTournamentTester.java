3bbf6dc1af4ea31532e9dd5e00eaf604245e5cbd
Hariesh R <86884890+HarieshR@users.noreply.github.com>
2023-02-06 13:07:28 +0530

    Add files via upload

---
+++ assessment/assessment01/assessment 01/DXCCricketTournamentTester.java
@@ -0,0 +1,8 @@
+package assessment;
+public class DXCCricketTournamentTester {
+	public static void main(String[] args) {
+	    DXCCricketTournament in = new DXCCricketTournament();
+	    in.DXCCricketTournamentDetails();
+	    }
+	}
+
